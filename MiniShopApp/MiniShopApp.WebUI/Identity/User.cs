72f3d3515c5159ce629c0a22986537e8a2537fa1
gulsaherturk <gulsaherturk58@gmail.com>
2022-05-26 13:11:35 +0300

    Login işlemi

---
+++ MiniShopApp/MiniShopApp.WebUI/Identity/User.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniShopApp.WebUI.Identity
+{
+    public class User:IdentityUser
+    { //haızr tablolar yaratıldı üzerine ek olarak user tablosunda bu alamların olmasınıda istediğimi belirtttim.
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+}
